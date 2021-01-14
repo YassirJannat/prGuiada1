602641b6121dbd59813766dc675854fe8beaf941
alumne <alumne@alumnepc>
2021-01-14 12:03:18 +0100

    a

--- prGuiada1/src/com/alumne/prguiada1/PrGuiada1.java
+++ prGuiada1/src/com/alumne/prguiada1/PrGuiada1.java
@@ -16,7 +16,7 @@ bw.newLine();
 bw.write(" </title>");bw.newLine();
 bw.write(" </head>");bw.newLine();
 bw.write(" <body>");bw.newLine();
-bw.write(" Projecte inicial yassir j");
+bw.write(" Projecte inicial");
 bw.newLine();
 bw.write(" </body>");bw.newLine();
 bw.write("</html>");bw.newLine();
